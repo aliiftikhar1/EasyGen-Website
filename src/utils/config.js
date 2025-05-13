2f7caf4f379f879a225d05d6d4b25b7fe948a48e
aliiftikhar1 <aj829077@gmail.com>
2025-05-13 15:26:58 +0500

    url added in config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,6 @@
-// utils/config.js
-export const API_URL = 'https://easygen-backend.onrender.com';
+export const API_URL = "https://easygen-backend.onrender.com"
 
-export const getApiUrl = (endpoint) => `${API_URL}${endpoint}`;
+export const getApiUrl = (endpoint) => {
+  console.log(API_URL)
+  return `${API_URL}${endpoint}`
+} 
