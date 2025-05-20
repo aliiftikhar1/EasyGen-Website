182eb0a3cc7d06e38ac652276c7ab383313e7ddd
aliiftikhar1 <aj829077@gmail.com>
2025-05-20 21:42:56 +0500

    link updation

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,7 @@
 // export const API_URL = "https://easygen-backend.onrender.com"
 // export const API_URL = "http://localhost:8000"
-export const API_URL = "http://trylenoxinstruments.com"
+// export const API_URL = "http://trylenoxinstruments.com"
+export const API_URL = "http://45.139.227.241"
 export const getApiUrl = (endpoint) => {
   console.log(API_URL)
   return `${API_URL}${endpoint}`
